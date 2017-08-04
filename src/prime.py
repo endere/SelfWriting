f92f175e81acb796173edb79f2398e893d5dc1f9
Erik <erik.end@gmail.com>
2017-08-04 08:30:47 -0700

    functional file writing

--- src/prime.py
+++ src/prime.py
@@ -2,10 +2,24 @@ import primedict
 
 
 def get_prime(index):
-    return primedict.primes[index]
+    try:
+        return primedict.primes[index]
+    except KeyError:
+        with open('primedict.py', 'r+') as file:
+            lines = file.readlines()
+            file.seek(0)
+            for i in lines:
+                if i != '}':
+                    file.write(i)
+                else:
+                    file.write('4:5, \n')
+                    file.write('}')
+            file.truncate()
+            file.close()
 
 
 
 
 if __name__ == '__main__':
-    print(get_prime(1))
+    for i in range(1, 6):
+        print(get_prime(i))
